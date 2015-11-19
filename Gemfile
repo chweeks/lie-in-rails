7e31e1c902c441e528fbbf33b454a6937ae8e641
chweeks <chrisharryweeks@gmail.com>
2015-11-19 21:05:09 +0000

    added dotenv

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'pg'
 
 gem 'httparty'
 
+gem 'dotenv-rails'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -3,7 +3,8 @@ class StatusesController < ApplicationController
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
   def show
-    response = HTTParty.get("https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=False&app_id=#{APP_ID}&app_key=e=#{APP_KEY}")
+    response = HTTParty.get("https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=False&app_id=#{Rails.application.secrets.app_id}&app_key=e=#{Rails.application.secrets.app_key}")
+    p response
     render json: response, status: 200
   end
 end
